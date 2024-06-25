7a81be9d68d8ef22bef5d1b398c61b25161a920f
debankanmitra <debankanmitra@gmail.com>
2024-06-25 17:32:17 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,69 @@
-from api.routes import app
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile, Form
+from api.anime import dreamshaperXL,face2paint
+from api.restore import restoreimg,cloudinary_upload
+from api.removebg import Background_Removal
+from api.generate import limewire,Item
+from api.outpaint import segmindOutpaint
+from api.upscale import photai_upscale
+from api.inpaint import segmindInpaint
 import uvicorn
+
+
+app = FastAPI()
+
+@app.post("/generate")
+def generate(item: Item):
+    response = limewire(item.style, item.prompt, item.negative_prompt)
+    return {"output": response['url']}
+
+
+@app.post("/upscale")
+def upscale(image: UploadFile = File(...)):
+    file = cloudinary_upload(image)
+    response = photai_upscale(file['url'],file['name'])
+    return {"output": response}
+
+@app.post("/inpainting")
+def inpainting(image: UploadFile = File(...),mask: UploadFile = File(...),prompt: str = Form(...),negative_prompt: str = Form(None)):
+    image = cloudinary_upload(image)
+    mask = cloudinary_upload(mask)
+    response = segmindInpaint(image['url'],mask['url'],prompt,negative_prompt)
+    return {"output": response}
+
+@app.post("/outpainting")
+def outpainting(image: UploadFile = File(...),prompt: str = Form(...),negative_prompt: str = Form(None)):
+    file = cloudinary_upload(image)
+    response = segmindOutpaint(file['url'], prompt, negative_prompt)
+    return {"output": response}
+
+@app.post("/removebg")
+def removebg(image: UploadFile = File(...)):
+    file = cloudinary_upload(image)
+    response = Background_Removal(file['url'])
+    return {"output": response}
+
+@app.post("/restore")
+def restore(image: UploadFile = File(...)):
+    imgfile = cloudinary_upload(image)
+    response = restoreimg(imgfile['url'],imgfile['name'])
+    return {"output": response['data']['2k']['url']}
+
+@app.post("/toanime")
+def toanime(image: UploadFile = File(...)):
+    response_url = face2paint(image)
+    return {"output": response_url['body']['imageUrl']}
+
+
+
+
+
+
+
+
+
+
+
+
 
 # origins = [
 #     "http://localhost.tiangolo.com",
@@ -19,12 +82,5 @@ import uvicorn
 #     allow_headers=["*"],
 # )
 
-# When a Python file is run directly as a script, the special variable __name__ is set to "__main__" within that script. 
-# This allows you to determine whether the script is being run as the main program or if it is being imported as a module
-# into another program.
-
-# By placing this code inside the if __name__ == "__main__": block, it ensures that the FastAPI application 
-# is only run when the script is executed directly
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000,reload=True)
+    uvicorn.run(app)
